bccaf553509d46ae64b4cef063d532191f4d6e56
Raisya Malik Gibran <143887181+Kanners-glitch@users.noreply.github.com>
2025-06-25 21:34:47 +0700

    Add files via upload

---
+++ Username validator/script.js
@@ -0,0 +1,17 @@
+const myText = document.getElementById("myText");
+const mySubmit = document.getElementById("mySubmit");
+const output = document.getElementById("output");
+
+mySubmit.onclick = function () {
+    let userName = myText.value.trim(); // ambil input + hapus spasi
+    let upperName = userName.toUpperCase(); // ubah jadi huruf besar semua
+    let nameLength = userName.length; // hitung panjang nama
+    let hasA = userName.toLowerCase().includes("a"); // cek apakah ada huruf 'a'
+
+    output.innerHTML = `
+        Nama kamu: ${userName} <br>
+        Huruf kapital: ${upperName} <br>
+        Panjang nama: ${nameLength} karakter<br>
+        Mengandung huruf "a": ${hasA ? "Iya" : "Tidak"}
+    `;
+}
