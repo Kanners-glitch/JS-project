379b016b425539d911bbfe344954355a4fd81d49
Raisya Malik Gibran <143887181+Kanners-glitch@users.noreply.github.com>
2025-06-30 00:09:39 +0700

    Add files via upload

---
+++ string method/slicing/script.js
@@ -0,0 +1,21 @@
+// const namaLengkap = "Raisya Malik Gibran"
+
+// // let bagian = namaLengkap.slice(0 , 6);
+// // console.log(bagian)
+
+// // let namaAkhir = namaLengkap.slice(6, 12)
+// // console.log(namaAkhir)
+
+// // let firstName = namaLengkap.slice(0, namaLengkap.indexOf(" "))
+// // let lastName = namaLengkap.slice(namaLengkap.indexOf(" ") + 1)
+
+// // console.log (firstName)
+// // console.log (lastName)
+
+// //latihan
+// const email = "bro1@gmail.com"
+
+// let userName = email.slice(0, email.indexOf("@"))
+// let extension = email.slice(email.indexOf("@") + 1)
+// console.log(userName)
+// console.log(extension)
